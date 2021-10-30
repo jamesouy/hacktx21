41b9df788be61c60363ce245e08489c653f84dcb
James Ouyang <52713371+jamesouyang2003@users.noreply.github.com>
2021-10-30 14:38:13 -0500

    Initial commit

---
+++ src/bot.ts
@@ -0,0 +1,13 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login("OTA0MDkwMzkyODI2MzU1NzUz.YX2dyg.zscL-pr4VzxHjC2vW75K_ZO5eZE");

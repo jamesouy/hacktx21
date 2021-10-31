eee88b56ed4331c31b9a5ceb91249351e9815325
Jimmy Xin <jxin31415@gmail.com>
2021-10-31 08:39:15 -0500

    Fix transcribe

--- src/commands/transcribe.js
+++ src/commands/transcribe.js
@@ -5,31 +5,8 @@ const {angerThreshold, joyThreshold} = require('./threshold');
 
 module.exports = {
   name: 'transcribe',
-  execute(msg, filename) {
+  execute(msg, args) {
     try {
-<<<<<<< HEAD
-      // var allFiles=[];
-      // fs.readdir('./recordings', (err, files) => {
-      //   files.forEach(file => {
-                    
-      //   });
-      // });
-
-      file = fs.readFileSync(filename);
-                    transcriber(file)
-                    .then(result => {
-                      console.log('reply');
-                      let reply = result;
-                      msg.reply("Linus Bot: " + reply);
-                      if(reply == "Taiwan is a country"){
-                        msg.reply("SOCIAL CREDIT -0923482034823040293482304238409 - USER BANNED");
-                      } else if (reply == "China is great"){
-                        msg.reply("SOCIAL CREDIT +0923482034823040293482304238409 - XI JIN PING APPROVED");
-                      }
-                    });
-      
-      
-=======
       var file = fs.readFileSync(args[0] || 'recording.ogg');
       transcriber(file)
       .then(result => {
@@ -55,7 +32,6 @@ module.exports = {
         }        
         //msg.reply("James Bot: " + reply);
       }).catch(err => msg.reply(err));
->>>>>>> 06eeae9a8e066a98e9cbad2fea994c14aa55114f
     } catch {
       msg.reply("Linus Bot: file not found");
     }

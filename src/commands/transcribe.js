1eeee2fee2af2a942219712144c08257ef7016bd
Jimmy Xin <jxin31415@gmail.com>
2021-10-31 00:38:06 -0500

    Didnt save

--- src/commands/transcribe.js
+++ src/commands/transcribe.js
@@ -7,7 +7,6 @@ module.exports = {
   name: 'transcribe',
   execute(msg, args) {
     try {
-<<<<<<< HEAD
       var file = fs.readFileSync(args[0] || 'recording.ogg');
       transcriber(file)
       .then(result => {
@@ -33,19 +32,6 @@ module.exports = {
         }        
         //msg.reply("James Bot: " + reply);
       }).catch(err => msg.reply(err));
-=======
-      var file = fs.readFileSync('./recordings/r'+args[0]+'.ogg');
-      transcriber(file)
-      .then(result => {
-        let reply = result;
-        msg.reply("Linus Bot: " + reply);
-        if(reply == "Taiwan is a country"){
-          msg.reply("SOCIAL CREDIT -0923482034823040293482304238409 - USER BANNED");
-        } else if (reply == "China is great"){
-          msg.reply("SOCIAL CREDIT +0923482034823040293482304238409 - XI JIN PING APPROVED");
-        }
-      });
->>>>>>> 066b64e342da9d4902fe42b55c6ca9dcc2bca1f7
     } catch {
       msg.reply("Linus Bot: file not found");
     }

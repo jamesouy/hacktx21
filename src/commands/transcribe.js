22eb84ef3c2b3f4d678ad8368522f7b61b1749a7
Linus Zheng <linuszheng@wireless-10-146-188-223.public.utexas.edu>
2021-10-31 00:21:00 -0500

    committing a merge conflict? we call that a commitment issue

--- src/commands/transcribe.js
+++ src/commands/transcribe.js
@@ -8,46 +8,45 @@ module.exports = {
   name: 'transcribe',
   execute(msg, args) {
     try {
-<<<<<<< HEAD
       var file = fs.readFileSync('./recordings/r'+args[0]+'.ogg');
       transcriber(file)
       .then(result => {
         let reply = result;
         msg.reply("Linus Bot: " + reply);
-        if(reply == "Taiwan is a country" || reply == "China is great"){
+        if(reply == "Taiwan is a country"){
           msg.reply("SOCIAL CREDIT -0923482034823040293482304238409 - USER BANNED");
+        } else if (reply == "China is great"){
+          msg.reply("SOCIAL CREDIT +0923482034823040293482304238409 - XI JIN PING APPROVED");
         }
       });
     } catch {
       msg.reply("Linus Bot: file not found");
     }
-=======
-      var file = fs.readFileSync(args[0] || 'recording.ogg');
-      transcriber(file)
-      .then(result => {
-        if (!result) throw new Error("no text");
-        toneParams = {
-          toneInput: {'text': result},
-          contentType: 'application/json',
-        };
-        toneAnalyzer.tone(toneParams)
-          .then(toneAnalysis => {
-            let reply = JSON.stringify(toneAnalysis.result, null, 2);
-            toneAnalysis.result.document_tone.tones.forEach(tone => {
-              if (tone.tone_id == "anger" && tone.score >= angerThreshold) {
-                reply += "\nYou sound angwy.";
-              } else if (tone.tone_id == "joy" && tone.score >= joyThreshold) {
-                reply += "\nYou sound joyous.";
-              }
-            });
-            msg.reply(result+"\n"+reply);
-          })
-        //msg.reply("James Bot: " + reply);
-      }).catch(err => msg.reply(err));
-    } catch {
-      msg.reply("File not found");
-    }
+    //   var file = fs.readFileSync(args[0] || 'recording.ogg');
+    //   transcriber(file)
+    //   .then(result => {
+    //     if (!result) throw new Error("no text");
+    //     toneParams = {
+    //       toneInput: {'text': result},
+    //       contentType: 'application/json',
+    //     };
+    //     toneAnalyzer.tone(toneParams)
+    //       .then(toneAnalysis => {
+    //         let reply = JSON.stringify(toneAnalysis.result, null, 2);
+    //         toneAnalysis.result.document_tone.tones.forEach(tone => {
+    //           if (tone.tone_id == "anger" && tone.score >= angerThreshold) {
+    //             reply += "\nYou sound angwy.";
+    //           } else if (tone.tone_id == "joy" && tone.score >= joyThreshold) {
+    //             reply += "\nYou sound joyous.";
+    //           }
+    //         });
+    //         msg.reply(result+"\n"+reply);
+    //       })
+    //     //msg.reply("James Bot: " + reply);
+    //   }).catch(err => msg.reply(err));
+    // } catch {
+    //   msg.reply("File not found");
+    // }
     
->>>>>>> 3695814744cdf60788654186e35422e6bcad5823
   }
 }

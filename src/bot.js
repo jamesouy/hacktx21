47b966625821fd12af9adf41d18c788475631302
James Ouyang <52713371+jamesouyang2003@users.noreply.github.com>
2021-10-31 09:12:14 -0500

    remove .env

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,6 @@
 // Require the necessary discord.js classes
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
 const fs = require('fs');
 const { Client, Intents } = require('discord.js');
 const Discord = require('discord.js');
@@ -8,7 +10,15 @@ require('dotenv').config();
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 client.login(process.env.BOT_TOKEN);
 
-// Initializing commands
+// Initializing slash commands
+const commands = [
+	new SlashCommandBuilder().setName('join').setDescription('Join your current channel'),
+	new SlashCommandBuilder().setName('leave').setDescription('Leave the voice channel'),
+	new SlashCommandBuilder().setName('autojoin').setDescription('Set whether the bot automatically joins channels'),
+].map(command => command.toJSON())
+const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
+
+// Initializing testing commands
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
@@ -16,7 +26,9 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-client.once('ready', () => console.log('The Bot is ready!'));
+client.once('ready', () => {
+	console.log('The Bot is ready!')
+});
 
 client.on('messageCreate', async (msg) => {
 

e97164541ee080399f686421017ecb02bc4f860a
Bowen Yin <bowenvictoryin@gmail.com>
2021-10-31 04:18:51 +0000

    Trancribe also analyzes

--- src/commands/transcribe.js
+++ src/commands/transcribe.js
@@ -1,6 +1,8 @@
 
 const fs = require('fs');
 const {transcriber} = require('../speechtotext');
+const {toneAnalyzer} = require('../analyzer');
+const {angerThreshold, joyThreshold} = require('./threshold');
 
 module.exports = {
   name: 'transcribe',
@@ -9,9 +11,25 @@ module.exports = {
       var file = fs.readFileSync(args[0]);
       transcriber(file)
       .then(result => {
-        let reply = result;
-        msg.reply("James Bot: " + reply);
-      })
+        if (!result) throw new Error("no text");
+        toneParams = {
+          toneInput: {'text': result},
+          contentType: 'application/json',
+        };
+        toneAnalyzer.tone(toneParams)
+          .then(toneAnalysis => {
+            let reply = JSON.stringify(toneAnalysis.result, null, 2);
+            toneAnalysis.result.document_tone.tones.forEach(tone => {
+              if (tone.tone_id == "anger" && tone.score >= angerThreshold) {
+                reply += "\nYou sound angwy.";
+              } else if (tone.tone_id == "joy" && tone.score >= joyThreshold) {
+                reply += "\nYou sound joyous.";
+              }
+            });
+            msg.reply(result+"\n"+reply);
+          })
+        //msg.reply("James Bot: " + reply);
+      }).catch(err => msg.reply(err));
     } catch {
       msg.reply("File not found");
     }
